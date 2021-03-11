fb45e53889dec40515909993e40d96b946868738
Jerome Caporossi <jerome.caporossi@gmail.com>
2021-03-11 14:37:00 +0100

    added smart contract for DID

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
-var SimpleStorage = artifacts.require("./SimpleStorage.sol");
+var EthereumDidRegistry = artifacts.require("./EthereumDidRegistry");
 
 module.exports = function(deployer) {
-  deployer.deploy(SimpleStorage);
+  deployer.deploy(EthereumDidRegistry);
 };
